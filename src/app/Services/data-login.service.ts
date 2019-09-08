d6fffe818e59a650a992936ff3219fbdb2c568ce
German Poblete <[mangerpv@gmail.com]>
2019-09-08 09:14:18 -0300

    fix last detail

--- src/app/Services/data-login.service.ts
+++ src/app/Services/data-login.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { userInterface } from 'src/app/Models/users';
+import { userInterface } from "src/app/Models/users";
 import { Router } from '@angular/router';
 
 @Injectable({
